e7247456278bc3996511ddcaa8dfc01ed2e9f0c9
ducvietnguyen286 <ducviet.nguyen286@gmail.com>
2026-08-04 17:31:10 +0700

    feat: update venue to Nha Hang Long Vi, 3A Dao Duy Anh, Ha Noi

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 21.02300, lng: 105.84320};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
