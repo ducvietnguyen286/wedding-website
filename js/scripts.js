0f067601a556ce9e26c80ebe34c5cb282125af2a
ducvietnguyen286 <ducviet.nguyen286@gmail.com>
2026-08-06 15:26:36 +0700

    style: enlarge header logo for clearer visibility

--- js/scripts.js
+++ js/scripts.js
@@ -115,7 +115,7 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "12px 0"
+                    "padding": "10px 0"
                 });
                 $('header .member-actions').css({
                     "top": "0",
@@ -127,7 +127,7 @@ $(document).ready(function () {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "18px 0"
+                    "padding": "14px 0"
                 });
                 $('header .member-actions').css({
                     "top": "0",
